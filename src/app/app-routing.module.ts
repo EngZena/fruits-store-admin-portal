d9519e8361ca5e6ac5cd550f66f837074d955685
engZena <zena_al_ja95@yahoo.com>
2022-06-20 22:15:57 +0300

    use-home-page-in-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomePageComponent } from './containers/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: HomePageComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
