820e11426334c1f1f032660cc03e9849a41bdb79
engZena <zena_al_ja95@yahoo.com>
2022-06-25 03:52:31 +0300

    declare-shared-module-and-import-it-in-app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { ContainersModule } from './containers/containers.module';
 import { NgModule } from '@angular/core';
+import { SharedModule } from './shared/Shared.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,6 +13,7 @@ import { NgModule } from '@angular/core';
     AppRoutingModule,
     BrowserAnimationsModule,
     ContainersModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
