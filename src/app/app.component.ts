0dbfce956a8b979ae2b5abc37273c140251fbd11
engZena <zena_al_ja95@yahoo.com>
2022-07-09 21:04:27 +0300

    use-network-service-to-render-no-internet-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NetworkService } from './core/services/Network/Network.service';
 import { Observable } from 'rxjs';
 import { ThemeService } from './core/services/theme/theme.service';
 
@@ -9,10 +10,17 @@ import { ThemeService } from './core/services/theme/theme.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  isOnline: boolean = true;
   isDarkTheme!: Observable<boolean>;
 
-  constructor(private themeService: ThemeService) {}
+  constructor(
+    private themeService: ThemeService,
+    private networkService: NetworkService
+  ) {}
   ngOnInit(): void {
+    this.networkService
+      .createOnline$()
+      .subscribe((isOnline: boolean) => (this.isOnline = isOnline));
     this.isDarkTheme = this.themeService.isDarkTheme;
   }
 }
