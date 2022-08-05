76f2633097791360dd870a8fc737ecfe7cbecb53
engZena <zena_al_ja95@yahoo.com>
2022-08-06 01:18:21 +0300

    refactor-imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { ComponentsModule } from '@components/components.module';
 import { ContainersModule } from '@containers/containers.module';
 import { CookieService } from 'ngx-cookie-service';
 import { EffectsModule } from '@ngrx/effects';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
 import { SharedModule } from './shared/Shared.module';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -22,7 +21,6 @@ import { StoreModule } from '@ngrx/store';
     AppRoutingModule,
     BrowserAnimationsModule,
     ContainersModule,
-    FlexLayoutModule,
     SharedModule,
     ComponentsModule,
     EffectsModule.forRoot([AuthEffects]),
