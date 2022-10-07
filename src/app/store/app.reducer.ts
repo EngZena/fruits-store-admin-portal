c544870df2d02f332dcca3370643d283d8580dee
engZena <zena_al_ja95@yahoo.com>
2022-10-08 00:15:21 +0300

    feat: declare-products-model

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -1,11 +1,14 @@
 import * as fromAuth from '@containers/login-page/store/auth.reducer';
+import * as fromProducts from '@containers/products/store/products.reducer';
 
 import { ActionReducerMap } from '@ngrx/store';
 
 export interface AppState {
   auth: fromAuth.State;
+  products: fromProducts.productsState;
 }
 
 export const appReducer: ActionReducerMap<AppState> = {
   auth: fromAuth.authReducer,
+  products: fromProducts.productsReducers,
 };
