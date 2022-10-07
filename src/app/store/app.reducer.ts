6efd90d142e082e7bb28a8702373813f561558db
engZena <zena_al_ja95@yahoo.com>
2022-10-08 00:19:02 +0300

    feat: ngrx-import-products-reducers-in-the-app-reducer

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -4,11 +4,11 @@ import * as fromProducts from '@containers/products/store/products.reducer';
 import { ActionReducerMap } from '@ngrx/store';
 
 export interface AppState {
-  auth: fromAuth.State;
   products: fromProducts.productsState;
+  auth: fromAuth.State;
 }
 
 export const appReducer: ActionReducerMap<AppState> = {
-  auth: fromAuth.authReducer,
   products: fromProducts.productsReducers,
+  auth: fromAuth.authReducer,
 };
