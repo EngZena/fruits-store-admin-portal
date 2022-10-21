00e4aa84e063515cab5264765276b3397c554d95
engZena <zena_al_ja95@yahoo.com>
2022-10-22 02:04:39 +0300

    ngcc: create-new-product-form-style-and-functional

--- src/app/containers/products/AddNewProduct/add-new-product.component.ts
+++ src/app/containers/products/AddNewProduct/add-new-product.component.ts
@@ -1,3 +1,5 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-new-product.component.scss'],
 })
 export class AddNewProductComponent {
+  productForm: FormGroup = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    price: new FormControl(null, [Validators.required]),
+    FruitType: new FormControl(null, [Validators.required]),
+    image: new FormControl(null, [Validators.required]),
+    imageName: new FormControl(null, [Validators.required]),
+  });
+
+  invalidChars = ['-', '+', 'e', 'E'];
+
+  imageURL: any;
+  imageMsg: any;
+
+  FruitsTypes = [
+    { value: 'summerFruits', viewValue: 'Summer Fruits' },
+    { value: 'winterFruits', viewValue: 'Winter Fruits' },
+  ];
   constructor() {}
+
+  keyPressNumbersDecimal(event: KeyboardEvent) {
+    if (this.invalidChars.includes(event.key)) {
+      event.preventDefault();
+    }
+  }
+
+  openInput() {
+    document.getElementById('fileInput')!.click();
+  }
+
+  selectFile(imageInput: any) {
+    if (!imageInput.files[0] || imageInput.files[0].length == 0) {
+      this.imageMsg = 'You must select an image';
+      return;
+    }
+
+    const mimeType = imageInput.files[0].type;
+
+    if (mimeType.match(/image\/*/) == null) {
+      this.imageMsg = 'Only images are supported';
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(imageInput.files[0]);
+
+    reader.onload = _imageInput => {
+      this.imageMsg = 'Image uploaded successfully';
+      this.imageURL = reader.result;
+      this.productForm.get('image')?.setValue(reader.result);
+      this.productForm.get('imageName')?.setValue(imageInput.files[0].name);
+    };
+  }
+
+  addNewFruit() {}
 }
--- src/app/shared/Shared.module.ts
+++ src/app/shared/Shared.module.ts
@@ -9,6 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
@@ -25,6 +26,7 @@ const materialModules = [
   MatToolbarModule,
   MatInputModule,
   MatIconModule,
+  MatSelectModule,
 ];
 
 const angularModules = [
