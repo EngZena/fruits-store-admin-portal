54d024ad1c214ad77d741ec98f78af62230f98ac
engZena <zena_al_ja95@yahoo.com>
2022-11-05 10:38:28 +0200

    ngrx: log-product-actions-to-console

--- src/app/containers/products/store/products.action-types.ts
+++ src/app/containers/products/store/products.action-types.ts
@@ -1,7 +1,7 @@
-export const INITIALIZE_PRODUCTS = '[PRODUCTS] initialize products';
-export const SET_ALL_PRODUCTS = '[PRODUCTS] set all products';
-export const GET_ALL_PRODUCTS = '[PRODUCTS] get all products';
-export const ADD_NEW_PRODUCTS = '[PRODUCTS] add new products';
-export const GET_PRODUCT_BY_ID = '[PRODUCTS] get product by id';
-export const DELETE_PRODUCT_BY_ID = '[PRODUCTS] delete product by id';
-export const UPDATE_PRODUCT_BY_ID = '[PRODUCTS] update product by id';
+export const INITIALIZE_PRODUCTS = '[NGRX] [PRODUCTS] initialize products';
+export const SET_ALL_PRODUCTS = '[NGRX] [PRODUCTS] set all products';
+export const GET_ALL_PRODUCTS = '[NGRX] [PRODUCTS] get all products';
+export const ADD_NEW_PRODUCTS = '[NGRX] [PRODUCTS] add new products';
+export const GET_PRODUCT_BY_ID = '[NGRX] [PRODUCTS] get product by id';
+export const DELETE_PRODUCT_BY_ID = '[NGRX] [PRODUCTS] delete product by id';
+export const UPDATE_PRODUCT_BY_ID = '[NGRX] [PRODUCTS] update product by id';
