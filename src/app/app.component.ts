1989719309175f89f10ee396b5e5e528673b7752
engZena <zena_al_ja95@yahoo.com>
2022-11-19 08:20:48 +0300

    feat: Keep-the-saved-theme-even-if-the-user-refreshes-the-page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import * as fromApp from '@store/app.reducer';
 import * as fromAuthActions from '@containers/login-page/store/auth.actions';
 
-import { BehaviorSubject, Observable } from 'rxjs';
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 import { NetworkService } from '@core/services/Network/Network.service';
 import { Store } from '@ngrx/store';
@@ -13,20 +18,27 @@ import { ThemeService } from '@core/services/theme/theme.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterContentChecked {
   isOnline: boolean = true;
   isDarkTheme!: Observable<boolean>;
   isAuthenticated = new BehaviorSubject<boolean>(false);
   constructor(
     private store: Store<fromApp.AppState>,
     private themeService: ThemeService,
-    private networkService: NetworkService
+    private networkService: NetworkService,
+    private cdref: ChangeDetectorRef
   ) {}
+
+  ngAfterContentChecked(): void {
+    this.cdref.detectChanges();
+  }
+
   ngOnInit(): void {
     this.store.dispatch(new fromAuthActions.AutoLogin());
     this.networkService
       .createOnline$()
       .subscribe((isOnline: boolean) => (this.isOnline = isOnline));
+    this.themeService.getDarkTheme();
     this.isDarkTheme = this.themeService.isDarkTheme;
     this.store.select('auth').subscribe(data => {
       this.isAuthenticated.next(true);
