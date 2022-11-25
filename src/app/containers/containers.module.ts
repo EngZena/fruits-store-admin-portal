40154e824909f27e45c666a78c71996bbb32ebd3
engZena <zena_al_ja95@yahoo.com>
2022-11-26 00:37:07 +0300

    refactore: rename-edit-product-component

--- src/app/containers/containers.module.ts
+++ src/app/containers/containers.module.ts
@@ -1,7 +1,7 @@
 import { AddNewProductComponent } from './products/AddNewProduct/add-new-product.component';
 import { ComponentsModule } from '@components/components.module';
 import { CustomersComponent } from './customers/customers.component';
-import { EditProductComponent } from './products/EditProduct/EditProduct.component';
+import { EditProductComponent } from './products/EditProduct/edit.product.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { NgModule } from '@angular/core';
 import { ProductsComponent } from './products/products.component';
