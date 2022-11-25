1db659c47d26e5ca14819f2aad059aba8fcef572
engZena <zena_al_ja95@yahoo.com>
2022-11-26 00:33:49 +0300

    get-product-data-from-store-ngrx

--- src/app/containers/products/EditProduct/EditProduct.component.ts
+++ src/app/containers/products/EditProduct/EditProduct.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import * as fromApp from '../../../store/app.reducer';
+import * as fromProductsActions from '@containers/products/store/products.actions';
+
+import { Component, OnInit } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
+import { ProductModel } from '@core/models/FruitModel';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-edit-product',
   templateUrl: './editProduct.component.html',
   styleUrls: ['./editProduct.component.scss'],
 })
-export class EditProductComponent {
-  constructor() {}
+export class EditProductComponent implements OnInit {
+  productById!: ProductModel | null;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private store: Store<fromApp.AppState>
+  ) {}
+
+  ngOnInit() {
+    this.store.dispatch(
+      new fromProductsActions.GetProductById(
+        this.activatedRoute.snapshot.params['id']
+      )
+    );
+    this.store.select('products').subscribe(data => {
+      this.productById = data.productById;
+    });
+  }
 }
