884dc3007d51a3362d38e28233fbd91793bc99e8
engZena <zena_al_ja95@yahoo.com>
2022-11-25 23:49:44 +0300

    feat: create-edit-product-component

---
+++ src/app/containers/products/EditProduct/EditProduct.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-product',
+  templateUrl: './editProduct.component.html',
+  styleUrls: ['./editProduct.component.scss'],
+})
+export class EditProductComponent {
+  constructor() {}
+}
