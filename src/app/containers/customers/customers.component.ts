fed65c51e3676314daa11525ef5661712e8830c7
engZena <zena_al_ja95@yahoo.com>
2022-11-28 22:30:28 +0300

    feat: setup-get-and-set-customer-using-local-storage

--- src/app/containers/customers/customers.component.ts
+++ src/app/containers/customers/customers.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { ActivatedRoute } from '@angular/router';
 import { Component } from '@angular/core';
 import { adminEmail } from '@containers/login-page/admin.data';
@@ -12,7 +13,25 @@ export class CustomersComponent {
   customersDataExist: boolean = false;
   showDetails: boolean = false;
   constructor(private route: ActivatedRoute) {
-    this.getCustomersData();
+    if (localStorage.getItem('customers_data') === null) {
+      this.getCustomersData();
+      localStorage.setItem(
+        'customers_data',
+        JSON.stringify(this.customersList)
+      );
+      console.log(
+        '\x1b[36m%s\x1b[0m',
+        '[LOCAL STORAGE] [Customers] set customers data in local storage'
+      );
+    } else {
+      this.setCustomersList(
+        JSON.parse(localStorage.getItem('customers_data')!)
+      );
+      console.log(
+        '\x1b[36m%s\x1b[0m',
+        '[LOCAL STORAGE] [Customers] get customers data from local storage'
+      );
+    }
   }
 
   getCustomersData() {
@@ -20,17 +39,21 @@ export class CustomersComponent {
       let result: any[] = [];
       if (data.customersData.length > 0) {
         result = [...data.customersData];
-        this.customersDataExist = true;
-        result.forEach(element => {
-          element = {
-            ...element,
-            showDetails: false,
-          };
-          this.customersList.push(element);
-        });
-        this.removeAdminFromCustomersList();
+        this.setCustomersList(result);
       }
     });
+  }
+
+  setCustomersList(customersData: any[]) {
+    this.customersDataExist = true;
+    customersData.forEach((element: any) => {
+      element = {
+        ...element,
+        showDetails: false,
+      };
+      this.customersList.push(element);
+    });
+    this.removeAdminFromCustomersList();
   }
 
   removeAdminFromCustomersList() {
