b4b490cfd4fed56afaddef9f13da38cf2ea4f981
engZena <zena_al_ja95@yahoo.com>
2022-12-24 03:20:01 +0300

    feat(service): get-customer-data-from-the-backend-after-resetting-it-in-local-storage-and-saving-it

--- src/app/core/services/localStorage/customers.local-storage.service.ts
+++ src/app/core/services/localStorage/customers.local-storage.service.ts
@@ -1,10 +1,40 @@
+/* eslint-disable no-console */
+import { CustomerModel } from '@core/models/cusromer.model';
+import { CustomersService } from '../apis/customers.service';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CustomersLocalStorageService {
+  constructor(private customersService: CustomersService) {}
+
   resetCustomersData() {
     localStorage.removeItem('customers_data');
+    console.log(
+      '\x1b[36m%s\x1b[0m',
+      '[LOCAL STORAGE] [Customers] remove customers data from local storage'
+    );
+    this.customersService
+      .getCustoemrsData()
+      .subscribe((result: CustomerModel[]) => {
+        this.setCustomersData(result);
+      });
+  }
+
+  setCustomersData(customersList: CustomerModel[]) {
+    localStorage.setItem('customers_data', JSON.stringify(customersList));
+    console.log(
+      '\x1b[36m%s\x1b[0m',
+      '[LOCAL STORAGE] [Customers] set customers data to local storage'
+    );
+  }
+
+  getCustomersData(): CustomerModel[] {
+    console.log(
+      '\x1b[36m%s\x1b[0m',
+      '[LOCAL STORAGE] [Customers] get customers data from local storage'
+    );
+    return JSON.parse(localStorage.getItem('customers_data')!);
   }
 }
